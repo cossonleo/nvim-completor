02bc60115c0aae19c2ac54932070f30979d55803
cossonleo <cossonleo@foxmail.com>
2020-03-16 08:18:30 +0800

    start snippet

---
+++ drop/head-fuzzy-match.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------
+--    LICENSE: MIT
+--     Author: Cosson2017
+--    Version: 0.3
+-- CreateTime: 2018-09-29 17:07:06
+-- LastUpdate: 2018-09-29 17:07:06
+--       Desc: head fuzzy match
+--------------------------------------------------
+
+local module = {}
+
+return module
